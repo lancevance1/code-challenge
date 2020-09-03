a04ee2ff6eb6c736571e956ab830b8eae564f9d3
Josh Young <jny986@gmail.com>
2020-09-03 11:07:43 +0930

    Updates
    Updates of composer & npm packages plus update to new stack changes

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,33 +8,6 @@ import Vue from 'vue'
 import { InertiaApp } from '@inertiajs/inertia-vue'
 Vue.use(InertiaApp)
 
-/**
- * Import and setup Vuetify
- */
-import Vuetify from 'vuetify'
-import "vuetify/dist/vuetify.min.css";
-import '@mdi/font/css/materialdesignicons.css'
-Vue.use(Vuetify)
-
-const vuetify = new Vuetify({
-    theme: {
-        themes: {
-            light: {
-                primary: '#1b588c',
-                secondary: '#2196f3',
-                accent: '#00bcd4',
-                error: '#f44336',
-                warning: '#ffc107',
-                info: '#03a9f4',
-                success: '#4caf50'
-            }
-        }
-    },
-    icons: {
-        iconfont: 'mdi'
-    }
-})
-
 let app = document.getElementById('app')
 
 new Vue({
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
 const mix = require('laravel-mix');
+const path = require("path");
+const tailwindcss = require('tailwindcss');
 
 /*
  |--------------------------------------------------------------------------
@@ -10,6 +12,23 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js("resources/js/app.js", "public/js")
+    .sass("resources/sass/app.scss", "public/css")
+    .options({
+        processCssUrls: false,
+        postCss: [tailwindcss("tailwind.config.js")]
+    })
+    .webpackConfig({
+        output: { chunkFilename: "js/[name].js?id=[chunkhash]" },
+        resolve: {
+            alias: {
+                vue$: "vue/dist/vue.runtime.esm.js",
+                "@": path.resolve("resources/js")
+            }
+        }
+        // plugins: webpackPlugins
+    })
+    .babelConfig({
+        plugins: ["@babel/plugin-syntax-dynamic-import"]
+    })
+    .version();
